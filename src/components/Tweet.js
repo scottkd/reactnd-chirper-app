19883e1f30fd06d8e703b5756dd3e8dc61e960a2
Scott deRegt <scottderegt@gmail.com>
2018-09-06 23:13:47 -0700

    implement like tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,11 +4,18 @@ import {formatTweet, formatDate} from "../utils/helpers"
 import {TiArrowBackOutline} from 'react-icons/ti/index'
 import {TiHeartOutline} from 'react-icons/ti/index'
 import {TiHeartFullOutline} from 'react-icons/ti/index'
+import {handleToggleTweet} from "../actions/tweets"
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault()
-    // todo: store liking of tweet
+
+    const {dispatch, tweet, authedUser} = this.props
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser,
+    }))
   }
   toParent = (e, id) => {
     e.preventDefault()
