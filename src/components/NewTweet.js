c937ed08b4f6fcb8dd496ee823367967fdec2cba
Scott deRegt <scottderegt@gmail.com>
2018-09-06 23:42:26 -0700

    new tweet logic

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {handleAddTweet} from "../actions/tweets"
 
 class NewTweet extends Component {
   state = {
@@ -15,10 +17,9 @@ class NewTweet extends Component {
     e.preventDefault()
 
     const {text} = this.state
+    const {dispatch, id} = this.props
 
-    //todo: add tweet to store
-
-    console.log('New Tweet: ', text)
+    dispatch(handleAddTweet(text, id))
 
     this.setState(() => {
       text: ''
@@ -57,4 +58,4 @@ class NewTweet extends Component {
   }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
