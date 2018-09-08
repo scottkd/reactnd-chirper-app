6894afb0335f12a10842c1196ef9e94aa7f7d1c0
Scott deRegt <scottderegt@gmail.com>
2018-09-08 11:22:59 -0700

    React Router

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {handleAddTweet} from "../actions/tweets"
+import {Redirect} from 'react-router-dom'
 
 class NewTweet extends Component {
   state = {
     text: '',
+    toHome: false,
   }
   handleChange = (e) => {
     const text = e.target.value
@@ -21,14 +23,19 @@ class NewTweet extends Component {
 
     dispatch(handleAddTweet(text, id))
 
-    this.setState(() => {
-      text: ''
-    })
+    this.setState(() => ({
+      text: '',
+      toHome: !id,
+    }))
   }
 
   render() {
-    const {text} = this.state
-    {/*todo: Redirect to home view if submitted*/}
+    const {text, toHome} = this.state
+
+    if (toHome === true) {
+      return <Redirect to='/' />
+    }
+
     const tweetLeft = 280 - text.length
     return (
       <div>
